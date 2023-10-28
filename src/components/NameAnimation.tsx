d8b0bcae9e03dcad777666af5cbee466615f248c
gitwithashishroy <ashish.roy@admitkard.com>
2023-10-28 16:46:05 +0530

    feat(codearoy): initialise the app

---
+++ src/components/NameAnimation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '.NameAnimation.module.scss';
+
+const NameAnimation: React.FC = () => {
+  return (
+    <div className={styles.nameAnimationContainer}>
+      <h1 className={styles.nameAnimation}>Ashish Kumar Roy</h1>
+    </div>
+  );
+};
+
+export default NameAnimation;
