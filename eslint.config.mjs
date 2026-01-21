c4df259d8be8fc908ee65f24ab41e10bf9cfe278
Ashish Kumar Roy <ashishroy7652@gmail.com>
2026-01-21 23:24:27 +0530

    chore: update ESLint and Prettier configurations, refine Husky hooks, and adjust TypeScript target

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,27 @@
-import nextPlugin from '@next/eslint-plugin-next';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-export default [
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'prettier'),
   {
-    ignores: ['node_modules', '.next', 'out'],
-  },
-  {
-    plugins: {
-      '@next/next': nextPlugin,
-    },
     rules: {
-      ...nextPlugin.configs['core-web-vitals'].rules,
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: true,
+        },
+      ],
     },
   },
 ];
+
+export default eslintConfig;
